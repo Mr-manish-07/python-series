bc84ba84e16bd6f40a27adff99ce64d5c112d595
Manish Kumar <manish825316@gmail.com>
2025-07-29 22:47:19 +0530

    fString

---
+++ String/fString.py
@@ -0,0 +1,8 @@
+info = "This is {} kumar and i am from {}"
+name = "Manish"
+city = "ranchi"
+print(info.format(name,city)) # Not good to go
+print(f"This is {name} kumar and i am from {city}")
+price = 45.5664
+print(f"The price is {price :20.2f} ")
+print(f"{3*4}")
