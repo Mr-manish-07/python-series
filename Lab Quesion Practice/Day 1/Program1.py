b882b425a737b592aa316f7749cc9967681da551
Manish Kumar <manish825316@gmail.com>
2025-07-30 23:33:52 +0530

    strted to solve question

---
+++ Lab Quesion Practice/Day 1/Program1.py
@@ -0,0 +1,11 @@
+# Find maximum , minimum and average of 3 number without using python method
+
+n1 = int (input("Enter A : "))
+n2 = int (input("Enter B : "))
+n3 = int (input("Enter C : "))
+
+if n1 > n2 :
+    if n1 > n3 :
+        print("n1 is greater")
+    elif n3 > n1 :
+        print("n3 is greater")
